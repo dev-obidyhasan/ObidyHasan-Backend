72a43e439c03b442254d6cd0c6c9d59ecaa9cc22
Dev. Obidy Hasan <dev.obidyhasan@gmail.com>
2025-10-03 15:19:07 +0600

    include Login With Email & Password API

--- src/modules/Auth/auth.service.ts
+++ src/modules/Auth/auth.service.ts
@@ -1,3 +1,10 @@
+import bcryptjs from "bcryptjs";
+import httpStatus from "http-status-codes";
+import { prisma } from "../../config/db";
+import AppError from "../../helper/AppError";
+import { createUserTokens } from "../../utils/userTokens";
+import { access } from "fs";
+
 const loginWithEmailAndPassword = async ({
   email,
   password,
@@ -5,7 +12,28 @@ const loginWithEmailAndPassword = async ({
   email: string;
   password: string;
 }) => {
-  return "User logged in";
+  const isUserExist = await prisma.profile.findUnique({
+    where: { email },
+  });
+
+  if (!isUserExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "User does not exist");
+  }
+
+  const isPasswordMatched = await bcryptjs.compare(
+    password,
+    isUserExist.password
+  );
+
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.UNAUTHORIZED, "Password is incorrect");
+  }
+
+  const userToken = createUserTokens(isUserExist);
+
+  return {
+    accessToken: userToken.accessToken,
+  };
 };
 
 export const AuthService = {
