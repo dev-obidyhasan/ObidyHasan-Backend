9719b487cfcec9c6b49960c15ecc95616228dd7f
Dev. Obidy Hasan <dev.obidyhasan@gmail.com>
2025-10-10 09:40:06 +0600

    changes some functionality

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,6 @@ app.use(
   cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 app.use(compression());
