5bf3d22914ccc867fe2230d823492ac4df8af2d1
Dev. Obidy Hasan <dev.obidyhasan@gmail.com>
2025-10-10 10:46:06 +0600

    fixed some issues

--- src/utils/setCookie.ts
+++ src/utils/setCookie.ts
@@ -1,30 +1,27 @@
 import { Response } from "express";
 
-export function setAuthCookie(
-  res: Response,
-  tokenInfo: { accessToken?: string },
-  email: string
-) {
-  const isProduction = process.env.NODE_ENV === "production";
-  const cookieDomain = isProduction ? process.env.FRONTEND_URL : undefined;
+export interface AuthToken {
+  accessToken?: string;
+}
 
+export const setAuthCookie = (
+  res: Response,
+  tokenInfo: AuthToken,
+  email: string
+) => {
   if (tokenInfo.accessToken) {
     res.cookie("accessToken", tokenInfo.accessToken, {
       httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction ? "none" : "lax",
-      domain: cookieDomain,
-      path: "/",
+      secure: true,
+      sameSite: "none",
     });
   }
-
   if (email) {
-    res.cookie("email", email, {
+    const encodedEmail = Buffer.from(email).toString("base64");
+    res.cookie("email", encodedEmail, {
       httpOnly: true,
-      secure: isProduction,
-      sameSite: isProduction ? "none" : "lax",
-      domain: cookieDomain,
-      path: "/",
+      secure: true,
+      sameSite: "none",
     });
   }
-}
+};
